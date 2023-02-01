6a8f830c3cca82862a3b3522904e078a6d05fcc7
mang <mang4371@gmail.com>
2023-02-01 19:14:15 +0900

    0201

---
+++ src/ProcessionPlus.java
@@ -0,0 +1,15 @@
+public class ProcessionPlus {
+
+    public static void main(String[] args){
+        solution(new int[][]{{4,5} , {2,3}} , new int[][]{{4,5} , {2,3}});
+    }
+    
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        
+
+        return answer;
+    }
+    
+}
